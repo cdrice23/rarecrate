6ae2ca9123b03a83b6e9893cec742aa6f38ac041
Chris Rice <cd.rice23@gmail.com>
2023-07-06 15:08:29 -0500

    Updated schema to remove database enums and moved to enum file in new core/constants dir.

---
+++ core/constants/enums/database.ts
@@ -0,0 +1,18 @@
+enum Role {
+  SUPERADMIN,
+  USER,
+}
+
+enum RequestStatus {
+  PENDING,
+  ACCEPTED,
+  REJECTED,
+}
+
+enum SocialPlatform {
+  TWITTER,
+  INSTAGRAM,
+  SPOTIFY,
+  YOUTUBE,
+  DISCOGS,
+}
