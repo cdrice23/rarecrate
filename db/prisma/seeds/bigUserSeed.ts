7b87b882ddf533e10810a6511c24c8a747017c2f
Chris Rice <cd.rice23@gmail.com>
2023-07-11 17:13:31 -0400

    Functional user + profile seed script.

--- db/prisma/seeds/bigUserSeed.ts
+++ db/prisma/seeds/bigUserSeed.ts
@@ -11,31 +11,103 @@ import { tagSeedArray as tags } from '@/core/constants/seedHelpers/tag';
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// // OG script to test users
+// export async function main() {
+//   // Iterate over the userSeedArray
+//   for (let user of users) {
+//     // Create the user in the database
+//     const createdUser = await prisma.user.create({
+//       data: {
+//         email: user.email,
+//         emailVerified: user.emailverified,
+//         role: user.role,
+//       },
+//     });
+
+//     // Find the corresponding profile in the profileSeedArray
+//     const matchedProfile = profiles.find(p => p.userid === user.id);
+
+//     if (matchedProfile) {
+//       // Check if a matching profile exists in the database based on certain fields
+//       const existingProfile = await prisma.profile.findFirst({
+//         where: {
+//           username: matchedProfile.username,
+//           isPrivate: matchedProfile.isprivate,
+//           bio: matchedProfile.bio,
+//         },
+//       });
+
+//       if (existingProfile) {
+//         // Connect the existing profile to the user
+//         await prisma.user.update({
+//           where: { id: createdUser.id },
+//           data: {
+//             profiles: { connect: { id: existingProfile.id } },
+//           },
+//         });
+//       } else {
+//         // Create a new Profile associated with the User in the database with auto-generated id
+//         await prisma.profile.create({
+//           data: {
+//             username: matchedProfile.username,
+//             isPrivate: matchedProfile.isprivate,
+//             bio: matchedProfile.bio,
+//             user: { connect: { id: createdUser.id } },
+//           },
+//         });
+//       }
+//     }
+//   }
+// }
+
+// Write a function to find the created X in the array Y to access all the extra props
+// Find all the related items you need
+// Create and connect
+
 export async function main() {
+  // Iterate over the userSeedArray
   for (let user of users) {
-    await prisma.user.create({
+    // Create the user in the database
+    const createdUser = await prisma.user.create({
       data: {
         email: user.email,
         emailVerified: user.emailverified,
         role: user.role,
       },
-      // profiles: {
-      //   connectOrCreate: profiles.map(profile => ({
-      //     where: { hname: profile.hname },
-      //     create: {
-      //       username: profile.username,
-      //       bio: profile.bio
-      //     },
-      //   })),
-      // },
     });
+
+    // Find the corresponding profile in the profileSeedArray
+    const matchedProfile = profiles.find(p => p.userid === user.id);
+
+    if (matchedProfile) {
+      // Check if a matching profile exists in the database based on certain fields
+      const existingProfile = await prisma.profile.findFirst({
+        where: {
+          username: matchedProfile.username,
+          isPrivate: matchedProfile.isprivate,
+          bio: matchedProfile.bio,
+        },
+      });
+
+      if (existingProfile) {
+        // Connect the existing profile to the user
+        await prisma.user.update({
+          where: { id: createdUser.id },
+          data: {
+            profiles: { connect: { id: existingProfile.id } },
+          },
+        });
+      } else {
+        // Create a new Profile associated with the User in the database with auto-generated id
+        await prisma.profile.create({
+          data: {
+            username: matchedProfile.username,
+            isPrivate: matchedProfile.isprivate,
+            bio: matchedProfile.bio,
+            user: { connect: { id: createdUser.id } },
+          },
+        });
+      }
+    }
   }
 }
-
-main()
-  .catch(e => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
--- db/prisma/seeds/resetDb.ts
+++ db/prisma/seeds/resetDb.ts
@@ -2,8 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 export async function main() {
-  await prisma.user.deleteMany();
   await prisma.profile.deleteMany();
+  await prisma.user.deleteMany();
 }
 
 main()
