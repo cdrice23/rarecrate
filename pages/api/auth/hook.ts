19d1d85c0512204035765741f2ff7a1110869bc0
Chris Rice <cd.rice23@gmail.com>
2023-07-29 10:52:15 -0500

    Updated workflow file for nightly job to generate prisma client.

--- pages/api/auth/hook.ts
+++ pages/api/auth/hook.ts
@@ -4,7 +4,7 @@ import { prisma } from '../../../db/prismaClient';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, user, secret } = req.body;
-  console.log('User Details!', user);
+  console.log(req.body.user);
 
   if (req.method !== 'POST') {
     return res.status(403).json({ message: 'Method not allowed' });
