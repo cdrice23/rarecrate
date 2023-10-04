9f6e58294c935e7be1eebe3f0813b9939547cd69
Chris Rice <cd.rice23@gmail.com>
2023-10-04 17:30:11 -0500

    Added a placeholder Add New Profile button to profile selection in user settings. Still need to handle and test.

--- lib/molecules/UserProfileDropdown/UserProfileDropdown.tsx
+++ lib/molecules/UserProfileDropdown/UserProfileDropdown.tsx
@@ -4,24 +4,36 @@ import { Pane } from '@/lib/atoms/Pane/Pane';
 import { useLocalState } from '@/lib/context/state';
 import cx from 'classnames';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { Route } from '@/core/enums/routes';
 
 const UserProfileDropdown = ({ userProfiles }) => {
   const [selectedItem, setSelectedItem] = useState(null);
   const { usernameMain, setUsernameMain } = useLocalState();
-  const { isOpen, getToggleButtonProps, getMenuProps, getItemProps, highlightedIndex } = useSelect({
-    items: userProfiles,
-    itemToString: () => userProfiles.map(userProfile => userProfile.username),
-    selectedItem,
-    onSelectedItemChange: ({ selectedItem: newSelectedItem }) => {
-      setSelectedItem(newSelectedItem);
-      setUsernameMain(newSelectedItem.username);
-    },
-  });
 
-  const sortedUserProfiles = [...userProfiles].sort((a, b) => {
+  const router = useRouter();
+
+  const originalUserProfiles = [...userProfiles];
+  const sortedUserProfiles = [...originalUserProfiles].sort((a, b) => {
     if (a.username === usernameMain) return -1;
     if (b.username === usernameMain) return 1;
     return 0;
+  });
+  sortedUserProfiles.push({ isAddProfile: true });
+
+  const { isOpen, getToggleButtonProps, getMenuProps, getItemProps, highlightedIndex } = useSelect({
+    items: sortedUserProfiles,
+    itemToString: () => userProfiles.map(userProfile => userProfile.username),
+    selectedItem,
+    onSelectedItemChange: ({ selectedItem: newSelectedItem }) => {
+      if (newSelectedItem.isAddProfile) {
+        // router.push(Route.NewProfile)
+        console.log('Handle new profile creation');
+      } else {
+        setSelectedItem(newSelectedItem);
+        setUsernameMain(newSelectedItem.username);
+      }
+    },
   });
 
   const handleDeleteProfile = profile => {
@@ -37,33 +49,48 @@ const UserProfileDropdown = ({ userProfiles }) => {
       </div>
       <ul {...getMenuProps()} className={cx('dropdownMenu')}>
         {isOpen &&
-          sortedUserProfiles.map((userProfile, index) => (
-            <li
-              key={index}
-              style={highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}}
-              {...getItemProps({
-                index,
-                item: userProfile,
-                // onSelect: handleSelectProfile,
-              })}
-            >
-              <div className={cx('menuItem')}>
-                <div className={cx('username')}>
-                  <p>{userProfile.username}</p>
-                  {userProfile.username === usernameMain && <DotOutline />}
-                </div>
-                <button
-                  onClick={event => {
-                    event.stopPropagation();
-                    handleDeleteProfile(userProfiles[index]);
-                    // Add logic to delete profile
-                  }}
+          sortedUserProfiles.map((userProfile, index) => {
+            if (userProfile.isAddProfile) {
+              return (
+                <li key={index} {...getItemProps({ index, item: userProfile })}>
+                  <div
+                    className={cx('menuItem')}
+                    style={highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}}
+                  >
+                    Add New Profile
+                  </div>
+                </li>
+              );
+            } else {
+              return (
+                <li
+                  key={index}
+                  {...getItemProps({
+                    index,
+                    item: userProfile,
+                  })}
                 >
-                  <MinusCircle />
-                </button>
-              </div>
-            </li>
-          ))}
+                  <div
+                    className={cx('menuItem')}
+                    style={highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}}
+                  >
+                    <div className={cx('username')}>
+                      <p>{userProfile.username}</p>
+                      {userProfile.username === usernameMain && <DotOutline />}
+                    </div>
+                    <button
+                      onClick={event => {
+                        event.stopPropagation();
+                        handleDeleteProfile(userProfiles[index]);
+                      }}
+                    >
+                      <MinusCircle />
+                    </button>
+                  </div>
+                </li>
+              );
+            }
+          })}
       </ul>
     </Pane>
   );
